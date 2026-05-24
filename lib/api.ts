c829589291627e04952725f41b694f2390ca20d3
Ahmad Yogi Kulumul Ilham <yogiilham003@gmail.com>
2026-05-24 08:23:28 +0700

    base url

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-kubh.onrender.com/api",
+  baseURL: "https://backend-25rv.onrender.com/api",
   // baseURL: "http://127.0.0.1:8000/api",
   headers: {
     "Content-Type": "application/json",
